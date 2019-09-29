6756d7c765bc1fd8fac11883f08bf05b99f6b442
root <root@ip-172-31-86-215.ec2.internal>
2019-09-29 16:47:37 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,12 +15,6 @@ class MoviesController < ApplicationController
     
     if params[:sort_by]
       @sorting = params[:sort_by]
-    else
-      @sorting = session[:sort_by]
-    end
-    
-    if @sorting!=session[:sort_by]
-      session[:sort_by] = @sorting
     end
     
     if @sorting == 'title'

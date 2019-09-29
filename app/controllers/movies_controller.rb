032d6d8eebe23c17debaa0e92606a9f60d96d2d6
root <root@ip-172-31-86-215.ec2.internal>
2019-09-29 17:16:11 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
     
     if params[:ratings]
       @ratings_filter = params[:ratings].keys

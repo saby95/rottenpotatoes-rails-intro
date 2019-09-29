4e10e86d21ca2c7ca7923a00b91f4f5e0beb9a9e
root <root@ip-172-31-86-215.ec2.internal>
2019-09-29 17:05:05 +0000

    part 2 check

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
     
     if params[:sort_by]
       @sorting = params[:sort_by]

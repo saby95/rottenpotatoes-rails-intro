ea3594ffbab4053c20775ae9f73d929f6f3a8e36
root <root@ip-172-31-86-215.ec2.internal>
2019-09-29 16:19:05 +0000

    part 1 release date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,6 +26,9 @@ class MoviesController < ApplicationController
     if @sorting == 'title'
           @movies = @movies.order(@sorting)
           @title_sort = 'hilite'
+    elsif @sorting == 'release_date'
+          @movies = @movies.order(@sorting)
+          @release_sort = 'hilite'
     end
     
   end
